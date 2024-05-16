5022842fb0f8c7be66160fc93a2b81bde54d919b
Vocal <vocalnight@protonmail.com>
2024-05-16 20:49:05 -0300

    Linked list methods created.

--- Algorithms and data structures/LinkedList.cs
+++ Algorithms and data structures/LinkedList.cs
@@ -9,5 +9,104 @@ namespace Algorithms_and_data_structures
     public class LinkedList
     {
         public LinkedListNode FirstNode { get; set; }
+
+        public LinkedListNode FindNode(int index)
+        {
+            LinkedListNode node = FirstNode;
+            var currentIndex = 0;
+
+            while(currentIndex < index)
+            {
+                node = node.Next;
+                currentIndex++;
+
+                if (node == null)
+                {
+                    return null;
+                }
+            }
+
+            if (currentIndex == index)
+            {
+                return node;
+            }
+
+            return null;
+        }
+
+        public int? Search(int value)
+        {
+            LinkedListNode node = FirstNode;
+            var currentIndex = 0;
+
+            while (node.Next != null)
+            {
+                if (node.Value == value)
+                {
+                    return node.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public void Add(int index, int value)
+        {
+            LinkedListNode node = FirstNode;
+            var currentIndex = 0;
+            var newNode = new LinkedListNode();
+
+            if (index == 0)
+            {
+                newNode.Value = value;
+                newNode.Next = FirstNode;
+                FirstNode = newNode;
+                return;
+            }
+
+            while (currentIndex < index - 1)
+            {
+                node = node.Next;
+                currentIndex++;
+            }
+
+            if (currentIndex == index - 1)
+            {
+                newNode.Value = value;
+                newNode.Next = node.Next;
+
+                node.Next = newNode;
+            }
+        }
+
+        public void Remove(int index)
+        {
+            LinkedListNode node = FirstNode;
+            var currentIndex = 0;
+
+            if (index == 0)
+            {
+                FirstNode = node.Next;
+            }
+
+            while (currentIndex < index - 1)
+            {
+                if (node.Next == null)
+                {
+                    return;
+                }
+
+                node = node.Next;
+                currentIndex++;
+            }
+
+            if (currentIndex == index - 1)
+            {
+                var currentNode = node;
+                var deletedNode = node.Next;
+
+                currentNode.Next = deletedNode.Next;
+            }
+        }
     }
 }
